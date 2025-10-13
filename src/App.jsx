f7bc2674433027e434963a1fb0e660a93a3d5661
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2025-10-13 18:58:20 +0400

    Add maintenance page files (logo, video, app)

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Logo from "./Logo.png";
+
+export default function App() {
+  return (
+    <div className="relative w-full min-h-screen text-white">
+      <div className="absolute top-[15%] left-1/2 translate-x-[-50%] z-[2] pointer-events-none max-[650px]:w-[60%] max-[500px]:w-[80%]">
+        <img
+          src={Logo}
+          alt="logo"
+          className="mx-auto w-[450px] mb-20 max-[1280px]:mb-12 max-[650px]:w-auto"
+        />
+        <div className="text-white text-2xl text-center max-[1280px]:text-2xl max-[650px]:text-xl max-[500px]:text-lg">
+          Under Construction...
+        </div>
+      </div>
+      <div className="absolute z-[0] top-0 left-0 w-full h-full">
+        <video
+          src={`../src/video.mp4`}
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover bg-primary"
+        />
+      </div>
+      <ul className="absolute bottom-16 z-[2] w-full flex justify-center items-center flex-wrap text-lg gap-16 !gap-y-8 px-[5%]
+      max-[900px]:gap-12 max-[650px]:text-base max-[500px]:bottom-8">
+        <li>
+          <a href="mailto:boris@hb-links.com">boris@hb-links.com</a>
+        </li>
+        <li>
+          <a href="tel:+13474290201">+1 (347) 429-0201</a>
+        </li>
+        <li className="pointer-events-none">California, USA</li>
+      </ul>
+      <div className="absolute w-full h-full z-[1] bg-black/60 shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[15px]"></div>
+    </div>
+  );
+}
