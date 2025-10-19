f2a1234fac5dddcfb91ebe2bdca0d93ddc9dda84
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2025-10-19 22:08:40 +0400

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,29 @@ import Video from "../public/video.mp4";
 export default function App() {
   return (
     <div className="relative w-full min-h-screen text-white">
-      <div className="absolute z-[2] w-full min-h-screen flex flex-col justify-between items-center">
-        <div className="pt-[15%] pointer-events-none max-[650px]:w-[60%] max-[500px]:w-[80%] max-[650px]:pt-[25%]">
-          <img
-            src={Logo}
-            alt="logo"
-            className="mx-auto w-[450px] mb-20 max-[1280px]:mb-12 max-[650px]:w-auto max-[500px]:mb-20"
-          />
-          <div className="text-white text-2xl text-center max-[1280px]:text-2xl max-[650px]:text-xl">
-            Under Construction...
-          </div>
+      <div className="absolute top-[15%] left-1/2 translate-x-[-50%] z-[2] pointer-events-none max-[650px]:w-[60%] max-[500px]:w-[80%] 
+      max-[500px]:top-[10%]">
+        <img
+          src={Logo}
+          alt="logo"
+          className="mx-auto w-[450px] mb-20 max-[650px]:w-auto max-[500px]:mb-24"
+        />
+        <div className="text-white text-2xl text-center max-[1280px]:text-2xl max-[650px]:text-xl">
+          Under Construction...
         </div>
-        <ul
-          className="pt-20 pb-24 w-full flex justify-center items-center flex-wrap text-lg gap-16 !gap-y-8 px-[5%]
-      max-[900px]:gap-12 max-[650px]:text-lg max-[650px]:pb-12"
-        >
-          <li>
-            <a href="mailto:boris@hb-links.com">boris@hb-links.com</a>
-          </li>
-          <li>
-            <a href="tel:+13474290201">+1 (818) 303-3555</a>
-          </li>
-          <li className="pointer-events-none">California, USA</li>
-        </ul>
       </div>
+      <ul
+        className="absolute bottom-16 z-[2] w-full flex justify-center items-center flex-wrap text-lg gap-16 !gap-y-8 px-[5%]
+      max-[900px]:gap-12 max-[650px]:text-lg max-[500px]:bottom-12"
+      >
+        <li>
+          <a href="mailto:boris@hb-links.com">boris@hb-links.com</a>
+        </li>
+        <li>
+          <a href="tel:+13474290201">+1 (818) 303-3555</a>
+        </li>
+        <li className="pointer-events-none">California, USA</li>
+      </ul>
       <div className="absolute z-[0] top-0 left-0 w-full h-full">
         <video
           src={Video}
@@ -38,7 +37,7 @@ export default function App() {
           className="w-full h-full object-cover bg-primary"
         />
       </div>
-      <div className="absolute top-0 left-0 w-full h-full z-[1] bg-black/60 shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[15px]"></div>
+      <div className="absolute w-full h-full z-[1] bg-black/60 shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[15px]"></div>
     </div>
   );
 }
